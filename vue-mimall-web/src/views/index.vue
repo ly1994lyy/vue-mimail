bc0eac7e6c12c60fb2315487b13d8ab6f88f2b60
ly1994lyy <1141730852@qq.com>
2020-05-06 17:26:42 +0800

    广告&&商品列表

--- vue-mimall-web/src/views/index.vue
+++ vue-mimall-web/src/views/index.vue
@@ -4,57 +4,61 @@
       <div class="swiper-box">
         <div class="nav-menu">
           <ul class="menu-wrap">
-            <li class="menu-item" v-for="(menu,index) in menuList" :key="index">
+            <li
+              class="menu-item"
+              v-for="(menu, index) in menuList"
+              :key="index"
+            >
               <a href="javascript:;">
-                {{menu}}
+                {{ menu }}
                 <i class="iconfont icon-youbian"></i>
               </a>
               <div class="children">
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
                 <div class="child-item">
-                  <a href="">
-                    <img src="../assets/imgs/mi10.webp" alt="">
+                  <a href>
+                    <img src="../assets/imgs/mi10.webp" alt />
                     <span>小米10 青春版</span>
                   </a>
                 </div>
@@ -98,9 +102,213 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
-      <div class="product-box"></div>
+      <div class="ads-box">
+        <div class="span4">
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-shijian"></i>
+              小米秒杀
+            </a>
+          </div>
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-ziyuan"></i>
+              企业团购
+            </a>
+          </div>
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-Fma"></i>
+              F码通道
+            </a>
+          </div>
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-SIMCard"></i>
+              米粉卡
+            </a>
+          </div>
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-qian"></i>
+              以旧换新
+            </a>
+          </div>
+          <div class="home-channel">
+            <a href>
+              <i class="iconfont icon-huafeichongzhi"></i>
+              话费充值
+            </a>
+          </div>
+        </div>
+        <div class="ads-span3">
+          <a href>
+            <img
+              src="../assets/imgs/35a2239e10e392af73b6b7a737a039d6.jpg"
+              alt
+            />
+          </a>
+          <a href>
+            <img
+              src="../assets/imgs/35a2239e10e392af73b6b7a737a039d6.jpg"
+              alt
+            />
+          </a>
+          <a href>
+            <img
+              src="../assets/imgs/35a2239e10e392af73b6b7a737a039d6.jpg"
+              alt
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <div class="ads">
+          <a href="">
+            <img src="../assets/imgs/ads.webp" alt="" />
+          </a>
+        </div>
+        <div class="product-container">
+          <div class="product-header">
+            <div class="title">手机</div>
+            <div class="more">
+              <a href="">
+                查看全部
+                <i class="iconfont icon-youbian"></i>
+              </a>
+            </div>
+          </div>
+          <div class="product">
+            <div class="ads-img">
+              <a href=""><img src="../assets/imgs/shoujiads.webp" alt=""/></a>
+            </div>
+            <div class="product-list">
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+              <div class="product-detail">
+                <a href="">
+                  <img
+                    src="../assets/imgs/8729282b199b3ec51e31c1b6b15f3f93.webp"
+                    alt=""
+                  />
+                  <h3>小米10 青春版 5G</h3>
+                  <p class="desc">50倍潜望式变焦 / 轻薄5G手机</p>
+                  <p class="price">
+                    <span class="num">2099</span>
+                    元
+                    <span>起</span>
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -108,7 +316,7 @@
 <script>
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
-import {menuList} from "../utils/api"
+import { menuList } from "../utils/api";
 
 export default {
   data() {
@@ -117,58 +325,59 @@ export default {
       swiperOptions: {
         pagination: {
           el: ".swiper-pagination",
-          clickable:true
+          clickable: true,
         },
-        navigation:{
-          nextEl:'.swiper-button-next',
-          prevEl:'.swiper-button-prev'
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
-        autoplay:true,
-        loop:true
-      }
+        autoplay: true,
+        loop: true,
+      },
     };
   },
   components: {
     Swiper,
-    SwiperSlide
+    SwiperSlide,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/config.scss';
+@import "../assets/scss/config.scss";
+@import "../assets/scss/mixin.scss";
 
 .index {
   .swiper-box {
-    .nav-menu{
+    .nav-menu {
       position: absolute;
       height: 460px;
       box-sizing: border-box;
       width: 234px;
       z-index: 9;
       padding: 20px 0;
-      background-color: rgba(105,101,101,.6);
-      .menu-wrap{
-        .menu-item{
+      background-color: rgba(105, 101, 101, 0.6);
+      .menu-wrap {
+        .menu-item {
           height: 42px;
           line-height: 42px;
           cursor: pointer;
-          a{
+          a {
             font-size: 14px;
             color: $colorG;
             padding-left: 30px;
-            i{
+            i {
               position: absolute;
               right: 20px;
             }
           }
-          &:hover{
+          &:hover {
             background-color: $colorA;
-            .children{
+            .children {
               display: grid;
             }
           }
-          .children{
+          .children {
             min-width: 248px;
             height: 460px;
             position: absolute;
@@ -177,16 +386,16 @@ export default {
             background-color: $colorG;
             border: 1px solid #e0e0e0;
             display: none;
-            grid-template-columns: repeat(auto-fill,248px);
-            grid-template-rows: repeat(auto-fill,76px);
+            grid-template-columns: repeat(auto-fill, 248px);
+            grid-template-rows: repeat(auto-fill, 76px);
             grid-auto-flow: column;
             box-sizing: border-box;
-            .child-item{
+            .child-item {
               width: 248px;
               padding: 10px 0;
-              a{
+              a {
                 color: $colorB;
-                span{
+                span {
                   position: absolute;
                 }
               }
@@ -198,11 +407,156 @@ export default {
     .swiper-container {
       width: 100%;
       height: 460px;
-      img{
+      img {
         width: 100%;
       }
-      .swiper-button-prev{
+      .swiper-button-prev {
         left: 244px;
+      }
+    }
+  }
+  .ads-box {
+    margin-top: 14px;
+    display: flex;
+    justify-content: space-between;
+    .span4 {
+      width: 234px;
+      height: 170px;
+      box-sizing: border-box;
+      background-color: #5f5750;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+      .home-channel {
+        text-align: center;
+        padding-top: 18px;
+        a {
+          color: #fff;
+          opacity: 0.7;
+          font-size: 12px;
+          i {
+            font-size: 25px;
+            opacity: 0.7;
+            display: block;
+          }
+          &:hover{
+            color: #fff;
+            opacity: 1;
+            i{
+              opacity: 1;
+            }
+          }
+        }
+      }
+    }
+    .ads-span3 {
+      display: flex;
+      justify-content: space-between;
+      width: 978px;
+      height: 170px;
+      a {
+        width: 316px;
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+  .product-box {
+    width: 100%;
+    margin-top: 30px;
+    background-color: #f5f5f5;
+    .ads {
+      img {
+        width: 100%;
+      }
+    }
+    .product-container {
+      margin-top: 10px;
+      width: 100%;
+      height: 686px;
+      .product-header {
+        width: 100%;
+        height: 58px;
+        @include flex();
+        .title {
+          font-size: 22px;
+          font-weight: 200;
+          line-height: 58px;
+          color: #333;
+        }
+        .more {
+          font-size: 16px;
+          line-height: 58px;
+          a {
+            color: #333;
+            i {
+              background-color: #757575;
+              border-radius: 50%;
+              color: $colorG;
+            }
+          }
+          &:hover {
+            a {
+              color: red;
+              i {
+                background-color: red;
+              }
+            }
+          }
+        }
+      }
+      .product {
+        width: 100%;
+        display: flex;
+        .ads-img {
+          width: 234px;
+          height: 614px;
+          img {
+            height: 100%;
+          }
+          &:hover {
+            @include up1();
+          }
+        }
+        .product-list {
+          width: 992px;
+          height: 627px;
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+          grid-template-rows: 1fr 1fr;
+          justify-items: end;
+          .product-detail {
+            width: 234px;
+            height: 300px;
+            background-color: $colorG;
+            text-align: center;
+            &:hover {
+              @include up1();
+            }
+            img {
+              width: 160px;
+              height: 160px;
+              margin-top: 23px;
+            }
+            .title {
+              text-align: center;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              overflow: hidden;
+            }
+            .desc {
+              margin: 0 10px 10px;
+              height: 18px;
+              font-size: 12px;
+              color: #b0b0b0;
+            }
+            .price {
+              text-align: center;
+              color: #ff6700;
+            }
+          }
+        }
       }
     }
   }
